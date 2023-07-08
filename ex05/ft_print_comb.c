ea6eb283e5a4baad75bfb05b12ec0ed09e993d4a
Noah Droineau <ndroinea@z2r4p2.42lyon.fr>
2023-07-08 10:00:02 +0200

    C00

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -3,54 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_comb.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kgarcia <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: ndroinea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/05 20:55:45 by kgarcia           #+#    #+#             */
-/*   Updated: 2023/07/06 14:12:19 by ndroinea         ###   ########.fr       */
+/*   Created: 2023/07/06 15:00:06 by ndroinea          #+#    #+#             */
+/*   Updated: 2023/07/06 15:15:01 by ndroinea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_print_int(int a)
-{
-	char	*chiffre;
-
-	chiffre = "0123456789";
-	write(1, chiffre + a, 1);
-}
-
-void	ft_print_nums(int a, int b, int c)
-{
-	ft_print_int(a);
-	ft_print_int(b);
-	ft_print_int(c);
-}
-
 void	ft_print_comb(void)
 {
-	int	a;
-	int	b;
-	int	c;
+	char	num1;
+	char	num2;
+	char	num3;
 
-	a = 0;
-	while (a <= 7)
+	num1 = '0';
+	while (num1 <= '9')
 	{
-		b = a + 1;
-		while (b <= 8)
+		num2 = num1 + 1;
+		while (num2 <= '9')
 		{
-			c = b + 1;
-			while (c <= 9)
+			num3 = num2 + 1;
+			while (num3 <= '9')
 			{
-				ft_print_nums(a, b, c);
-				if (a != 7 || b != 8 || c != 9)
-				{
+				write(1, &num1, 1);
+				write(1, &num2, 1);
+				write(1, &num3, 1);
+				if (!(num1 == '7' && num2 == '8' && num3 == '9'))
 					write(1, ", ", 2);
-				}
-				c += 1;
+				num3++;
 			}
-			b += 1;
+			num2++;
 		}
-		a += 1;
+		num1++;
 	}
 }
--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -3,30 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kgarcia <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: ndroinea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/06 09:28:49 by kgarcia           #+#    #+#             */
-/*   Updated: 2023/07/06 11:20:53 by kgarcia          ###   ########.fr       */
+/*   Created: 2023/07/06 15:03:37 by ndroinea          #+#    #+#             */
+/*   Updated: 2023/07/06 15:10:40 by ndroinea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_print_comb2(void);
+#include <unistd.h>
 
-void putchar(char c)
+void	ft_print_comb2(void);
+
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
 void	ft_print_comb2(void)
 {
-	char a;
-	char b;
+	char	a;
+	char	b;
 
 	a = 0;
 	b = 0;
-
 	while (a <= 99)
 	{
 		b = a + 1;
 		while (b <= 99)
 		{
+			ft_putchar(a / 10 + '0');
+			ft_putchar(a % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(b / 10 + '0');
+			ft_putchar(b % 10 + '0');
+			b++;
+			if (a != 98)
+			{
+				write(1, ", ", 2);
+			}
+		}
+		a++;
+	}
+}
